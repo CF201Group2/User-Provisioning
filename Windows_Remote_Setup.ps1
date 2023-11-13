0f9fb44ba4292a0567306b7bd5caed112efee5d3
John Kelly <147424198+JFKELLY89@users.noreply.github.com>
2023-11-13 17:58:59 -0600

    Update Windows_Remote_Setup.ps1

--- Windows_Remote_Setup.ps1
+++ Windows_Remote_Setup.ps1
@@ -1,17 +1,26 @@
+# Script Name:                  Automated Endpoint Configuration
+# Author:                       John Kelly
+# Date of latest revision:      11/13/2023
+# Purpose:                      Create a Powershell script that automatically establishes settings to enable RDP and to remove bloatware
+
+# ---------------------Configure Sharing and allow Ping------------------------------
 # Enable File and Printer Sharing
-# Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
+Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
 
 # Allow ICMP traffic
 netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4
 
+# ---------------------Configure RDP ------------------------------
+
 # Enable Remote management
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
-
-# Remove bloatware
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat'))
 
 # Enable Hyper-V
  Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 
-# Disable SMBv1, an insecure protocol
-Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
+# Enable NLA
+Set-ItemProperty ‘HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\‘ -Name “UserAuthentication” -Value 1
+
+# --------------------------Remove Bloatware-------------------------
+# Remove bloatware
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat'))
