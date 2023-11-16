ec14843d8777c0da5aad63b7d9c01ba8ea928ad0
Juan Maldonado <77375345+Juan-bit94@users.noreply.github.com>
2023-11-15 21:18:35 -0800

    Add files via upload

---
+++ Backup-solution-v1.ps1
@@ -0,0 +1,27 @@
+﻿# This displays your username
+$UserName = $env:USERNAME
+Write-Host "Current username: $Username"
+
+# This sets up variables
+$BackupPath = "C:\Backups\$UserName"
+
+# This creates a backup folder if it doesn't exist
+if (-not(Test-Path $BackupPath -PathType Container)) {
+    New-Item -ItemType Directory -Path $BackupPath -Force
+}
+
+# This uses robocopy to backup files
+$SourcePath = "C:\users\$UserName\Documents"
+robocopy $SourcePath $BackupPath /E /COPY:DAT /R:0 /W:0 
+
+# This creates a system restore point
+$RestorePointDescription = "Backup for $UserName"
+
+# This invokes the Checkpoint-Computer cmdlet using PowerShell
+try {
+     Checkpoint-Computer -Description $RestorePointDescription -RestorePointType MODIFY_SETTINGS
+     Write-Host "System restore point created successfully."
+     } catch {
+         Write-Host "Failed to create system restore point: $_"
+     }
+Write-Host "Backup and checkpoint process completed."
